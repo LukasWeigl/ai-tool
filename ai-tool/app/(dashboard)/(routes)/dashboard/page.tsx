cd69563060a0903097bfcc9b16f1922c6068866a
LukasWeigl <63125168+LukasWeigl@users.noreply.github.com>
2023-07-31 16:41:44 +0200

    init commit

---
+++ ai-tool/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function DashboardPage() {
+  return (
+    <Button variant="destructive" size="lg">Click Me</Button>
+  )
+}
